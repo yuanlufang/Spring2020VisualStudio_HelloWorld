800b83c3b698634e3da2cce34d4401310d953e1a
yuanlufang@mail.usf.edu <yuanlufang@mail.usf.edu>
2020-01-26 21:26:18 -0500

    changes

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@ namespace Spring2020VisualStudio_HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Happy to serve you!");
+            Console.WriteLine("Modified by your best friend :)");
             Console.Write("Press SPACE to continue...");
+            Console.ReadKey(true);
 
         }
     }
