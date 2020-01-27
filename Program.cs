ee15d28bb83e819ab7d2737037f7b70919b1abc4
yuanlufang@mail.usf.edu <yuanlufang@mail.usf.edu>
2020-01-26 19:05:43 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Spring2020VisualStudio_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Happy to serve you!");
+            Console.Write("Press SPACE to continue...");
+
+        }
+    }
+}
